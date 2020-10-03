0ded64a1e8c561d491ae717e49f7f47a44c64b09
yosefmoq <yos.ail@outlook.com>
2020-10-03 22:22:47 +0300

    first commit

---
+++ app/src/main/java/com/yosefmoq/moamenproject/MainActivity.java
@@ -0,0 +1,12 @@
+package com.yosefmoq.moamenproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+        @Override
+        protected void onCreate(final Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main);
+        }
+}
