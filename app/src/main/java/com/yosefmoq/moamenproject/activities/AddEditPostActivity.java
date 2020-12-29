05404f99eec3dd69bc6b25ea7a4a2ad3e09f5205
Hamzashami <shamihamza25@gmail.com>
2020-12-29 03:58:31 +0200

    some edting for AddEditPostActivity

--- app/src/main/java/com/yosefmoq/moamenproject/activities/AddEditPostActivity.java
+++ app/src/main/java/com/yosefmoq/moamenproject/activities/AddEditPostActivity.java
@@ -3,7 +3,9 @@ package com.yosefmoq.moamenproject.activities;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+
 import com.nguyenhoanglam.imagepicker.model.Image;
+
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,7 +16,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import com.google.firebase.firestore.FirebaseFirestore;
-
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.nguyenhoanglam.imagepicker.ui.imagepicker.ImagePicker;
@@ -32,29 +33,30 @@ public class AddEditPostActivity extends AppCompatActivity {
     SweetAlertDialog sweetAlertDialog;
     EditText etPostText;
     Button btnAdd;
-    ImageView ivAdd,ivImage;
+    ImageView ivAdd, ivImage;
     String imageLink;
     FirebaseFirestore firebaseFirestore;
     private StorageReference mStorageRef;
     private static final int PERMISSION_CODE_READ_WRITE_EXTERNAL_STORAGE = 236;
-    ArrayList<Image> images= new ArrayList<>();
+    ArrayList<Image> images = new ArrayList<>();
     int type;
     Post postForEdit;
     int postId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_edit_post);
         firebaseFirestore = FirebaseFirestore.getInstance();
         findView();
-        type =getIntent().getIntExtra("type",0);
-        if(type == 0){
+        type = getIntent().getIntExtra("type", 0);
+        if (type == 0) {
             btnAdd.setText("Add");
-        }else {
+        } else {
             btnAdd.setText("Edit");
 
             sweetAlertDialog.show();
-            firebaseFirestore.collection("posts").document(getIntent().getIntExtra("postId",0)+"").get().addOnCompleteListener(runnable -> {
+            firebaseFirestore.collection("posts").document(getIntent().getIntExtra("postId", 0) + "").get().addOnCompleteListener(runnable -> {
                 sweetAlertDialog.hide();
                 postForEdit = runnable.getResult().toObject(Post.class);
                 Picasso.get().load(postForEdit.getImage()).into(ivImage);
@@ -76,7 +78,7 @@ public class AddEditPostActivity extends AppCompatActivity {
         btnAdd.setOnClickListener(view -> {
             sweetAlertDialog.show();
 
-            if(type==0) {
+            if (type == 0) {
                 if (images.size() == 0) {
                     Toast.makeText(this, "please choose image", Toast.LENGTH_SHORT).show();
                     return;
@@ -85,20 +87,20 @@ public class AddEditPostActivity extends AppCompatActivity {
                     Toast.makeText(this, "please Enter post text", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                StorageReference riversRef = mStorageRef.child("images/"+System.currentTimeMillis()+".jpg");
+                StorageReference riversRef = mStorageRef.child("images/" + System.currentTimeMillis() + ".jpg");
 
                 riversRef.putFile(images.get(0).getUri())
                         .addOnSuccessListener(taskSnapshot -> {
                             riversRef.getDownloadUrl().addOnSuccessListener(uri -> {
-                                imageLink =uri.toString();
+                                imageLink = uri.toString();
                                 Post post = new Post();
                                 FirebaseFirestore.getInstance().collection("posts").get().addOnCompleteListener(runnable -> {
-                                    int id =runnable.getResult().getDocuments().size()+1;
+                                    int id = runnable.getResult().getDocuments().size() + 1;
                                     post.setId(id);
                                     post.setImage(imageLink);
                                     post.setLike_count(0);
                                     post.setText(etPostText.getText().toString());
-                                    FirebaseFirestore.getInstance().collection("posts").document(id+"").set(post).addOnCompleteListener(runnable1 -> {
+                                    FirebaseFirestore.getInstance().collection("posts").document(id + "").set(post).addOnCompleteListener(runnable1 -> {
                                         sweetAlertDialog.hide();
                                         finish();
                                     });
@@ -107,20 +109,20 @@ public class AddEditPostActivity extends AppCompatActivity {
                         }).addOnFailureListener(e -> {
 
                 });
-            }else {
-                if(images.size()>0){
-                    StorageReference riversRef = mStorageRef.child("images/"+System.currentTimeMillis()+".jpg");
+            } else {
+                if (images.size() > 0) {
+                    StorageReference riversRef = mStorageRef.child("images/" + System.currentTimeMillis() + ".jpg");
 
                     riversRef.putFile(images.get(0).getUri())
                             .addOnSuccessListener(taskSnapshot -> {
                                 riversRef.getDownloadUrl().addOnSuccessListener(uri -> {
-                                    imageLink =uri.toString();
+                                    imageLink = uri.toString();
                                     Post post = new Post();
                                     FirebaseFirestore.getInstance().collection("posts").get().addOnCompleteListener(runnable -> {
                                         postForEdit.setImage(imageLink);
                                         postForEdit.setLike_count(0);
                                         postForEdit.setText(etPostText.getText().toString());
-                                        FirebaseFirestore.getInstance().collection("posts").document(postForEdit.getId()+"").set(post).addOnCompleteListener(runnable1 -> {
+                                        FirebaseFirestore.getInstance().collection("posts").document(postForEdit.getId() + "").set(post).addOnCompleteListener(runnable1 -> {
                                             sweetAlertDialog.hide();
                                             finish();
                                         });
@@ -130,9 +132,9 @@ public class AddEditPostActivity extends AppCompatActivity {
 
                     });
 
-                }else{
+                } else {
                     postForEdit.setText(etPostText.getText().toString());
-                    firebaseFirestore.collection("posts").document(postForEdit.getId()+"").set(postForEdit).addOnCompleteListener(task -> {
+                    firebaseFirestore.collection("posts").document(postForEdit.getId() + "").set(postForEdit).addOnCompleteListener(task -> {
                         sweetAlertDialog.hide();
                         finish();
                     });
@@ -140,24 +142,25 @@ public class AddEditPostActivity extends AppCompatActivity {
             }
 
 
-
         });
     }
 
     private void findView() {
-        sweetAlertDialog = new SweetAlertDialog(this,SweetAlertDialog.PROGRESS_TYPE);
+        sweetAlertDialog = new SweetAlertDialog(this, SweetAlertDialog.PROGRESS_TYPE);
 
         mStorageRef = FirebaseStorage.getInstance().getReference();
         etPostText = findViewById(R.id.etPostText);
-        btnAdd     = findViewById(R.id.btnAdd);
-        ivAdd      = findViewById(R.id.ivAdd);
-        ivImage    = findViewById(R.id.ivImage);
+        btnAdd = findViewById(R.id.btnAdd);
+        ivAdd = findViewById(R.id.ivAdd);
+        ivImage = findViewById(R.id.ivImage);
 
     }
+
     private boolean checkGetImagePermission() {
         int resultMic = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
         return resultMic == PackageManager.PERMISSION_GRANTED;
     }
+
     private void goToGallery() {
         if (checkGetImagePermission()) {
             ImagePicker.with(this)
@@ -182,7 +185,7 @@ public class AddEditPostActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(data!=null){
+        if (data != null) {
             images = data.getParcelableArrayListExtra("ImagePickerImages");
             ivImage.setImageURI(images.get(0).getUri());
 
